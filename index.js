39afda8155d6dc564c30a0f06a4677747c9bf3ac
Ben Gourley <bn@grly.me>
2016-12-01 10:56:20 +0000

    Call choo app.stop() when hot-swapping an app

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const defaultGlobalName = '__SAVED_APP_STATE__'
 
-module.exports = (globalName = defaultGlobalName) => ({
-  onStateChange: (state, data, prev, caller, createSend) => {
-    window[globalName] = state
-  },
-  wrapInitialState: (obj) => {
-    if (!window[globalName]) return obj
-    return window[globalName]
+module.exports = (app, globalName = defaultGlobalName) => {
+  if (window[`${globalName}__prev__`]) window[`${globalName}__prev__`].stop()
+  window[`${globalName}__prev__`] = app
+  return {
+    onStateChange: (state, data, prev, caller, createSend) => {
+      window[globalName] = state
+    },
+    wrapInitialState: (obj) => {
+      if (!window[globalName]) return obj
+      return window[globalName]
+    }
   }
-})
+}
