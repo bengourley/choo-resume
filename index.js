ab3a3f449a759eb42cd30a45c74cd7b1a7a73fbc
Marcelo Zapaia <qzapaia@gmail.com>
2017-03-23 10:36:56 -0300

    remove listeners

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const defaultGlobalName = '__SAVED_APP_STATE__'
+const defaultGlobalName = '__SAVED_APP_STATE__';
 
-module.exports = (globalName = defaultGlobalName) => (state, emitter) => {
-  Object.assign(state,window[globalName] || {});
+module.exports = (app, globalName = defaultGlobalName) => {
+  window[globalName] = window[globalName] || {};
 
-	emitter.on('*', function (messageName, data) {
-    window[globalName] = state;
+  app.use((state, emitter) => {
+    window[globalName].emitter && window[globalName].emitter.removeAllListeners();
+    window[globalName].emitter = emitter;
+    Object.assign(state, window[globalName].state || {});
+    emitter.on('*', function (messageName, data) {
+      window[globalName].state = state;
+    })
   })
+
+  return app;
 }
